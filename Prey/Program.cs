155aee4d8be0249ce97e22f5fe10e0a07df3af87
mashinyuza <62634948+mashinyuza@users.noreply.github.com>
2020-03-26 10:21:15 -0700

    feat: add new methods on Program and create HelloPayload class

--- Prey/Program.cs
+++ Prey/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Prey
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello MASHINYUZA!");
+            string json = @"{ 'who': 'World' }";
+
+            var who = FindTargetPerson(json);
+
+            Console.WriteLine($"Hello, {who}!");
         }
+
+        public static string FindTargetPerson(string json)
+        {
+            var payload = JsonConvert.DeserializeObject<HelloPayload>(json);
+            return payload.Who;
+        }
+    }
+
+    public class HelloPayload
+    {
+        public string Who { get; set; }
     }
 }
