1e677324729750d96bc6b182c7180f53d5ccacc6
mashinyuza <62634948+mashinyuza@users.noreply.github.com>
2020-03-26 10:15:17 -0700

    feat: add Prey code

---
+++ Prey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MASHINYUZA!");
+        }
+    }
+}
